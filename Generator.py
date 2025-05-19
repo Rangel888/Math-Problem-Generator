b06631891542f4270c337499111cd815f150ce5e
Rangel888 <rangelj@southwestern.edu>
2025-05-19 10:28:18 -0500

    Added comment for initilization of lights

--- Generator.py
+++ Generator.py
@@ -10,16 +10,19 @@ problem_answers = []
 with open('application.json', 'r') as f:
     config = ujson.load(f)
 
+
 def toggle_red_light():
     led1.toggle()
     utime.sleep(2)
     led1.toggle()  
-    
+
+
 def toggle_green_light():
     led2.toggle()
     utime.sleep(2)
     led2.toggle()
 
+# Initialize lights
 led1 = Pin(28, Pin.OUT)
 led2 = Pin(22, Pin.OUT)
 led1.low()
