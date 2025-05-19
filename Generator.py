097d9213287c996ab29b53b0f78339dd0ef20638
Rangel888 <rangelj@southwestern.edu>
2025-05-19 10:25:42 -0500

    Added fucntions to toggle lights

--- Generator.py
+++ Generator.py
@@ -4,8 +4,23 @@ import random as r
 import ujson
 
 problem_list = []
-
 problem_answers = []
 
+# Load configs
 with open('application.json', 'r') as f:
     config = ujson.load(f)
+
+def toggle_red_light():
+    led1.toggle()
+    utime.sleep(2)
+    led1.toggle()  
+    
+def toggle_green_light():
+    led2.toggle()
+    utime.sleep(2)
+    led2.toggle()
+
+led1 = Pin(28, Pin.OUT)
+led2 = Pin(22, Pin.OUT)
+led1.low()
+led2.low()
