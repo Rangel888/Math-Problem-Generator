850fa973d7085faa4135cffb045a6816d85f6ede
Rangel888 <rangelj@southwestern.edu>
2025-08-22 09:34:56 -0500

    adding additional configs

--- configs.py
+++ configs.py
@@ -9,7 +9,15 @@ class Config:
         
         # Set attributes with defaults if keys 
         self.number_of_problems = data.get('numberOfProblems', 5)
+        
         self.use_division_remainders = data.get('useDivisionRemainders', False)
 
+        self.difficulty_levels = data.get("difficultyLevels", {})
+
+        # Custom Free Play override
+        self.custom_ranges_enabled = data.get("customRanges", {}).get("enabled", False)
+        self.custom_range_operation = data.get("customRanges", {}).get("operation", "")
+        self.custom_range_min = data.get("customRanges", {}).get("min", 1)
+        self.custom_range_max = data.get("customRanges", {}).get("max", 10)
 
 
